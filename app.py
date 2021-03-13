8d3f21ca295663aa83a65c9a27684fd6134f9a64
Melissa Lowe <mel.c.lowe@gmail.com>
2021-03-13 13:47:30 -0600

    safety commit

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
 # Main code document for SQLAlchemy Homework
+
+# Import Flask
+from flask import Flask
+
+# Set up app
+app = Flask(__name__)
+
+# Establish necessary framework for webpage
+# Define static route
+@app.route("/")
+def index():
+    return "SQLAlchemy Homework"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Base needed code
+if __name__ == "__main__":
+    app.run(debug=True)
