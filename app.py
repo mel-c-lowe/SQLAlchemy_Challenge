52e49375ecc695b2bebfd551e3148940118ed0b7
Melissa Lowe <mel.c.lowe@gmail.com>
2021-03-13 14:49:06 -0600

    First query in app.py worked and jsonified

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 # Main code document for SQLAlchemy Homework
 
-# Import Flask
-from flask import Flask
+# Import Dependencies
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Set up interaction with the database
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Reflect tables into new model with automap_base
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# Set up reference to tables
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 # Set up app
 app = Flask(__name__)
@@ -10,7 +28,32 @@ app = Flask(__name__)
 # Define static route
 @app.route("/")
 def index():
-    return "SQLAlchemy Homework"
+    return (
+        f"SQLAlchemy Homework<br>"
+        f"/station<br>"
+        f"/measurement"
+    )
+
+@app.route("/station")
+def station():
+    # return "This is the station route"
+
+    # Start session to talk to the database
+    session = Session(engine)
+
+    """Return a list of stations"""
+    # Query all stations
+    results = session.query(Station.name).all()
+
+    # Close the session
+    session.close()
+
+    # Convert list of tuples into a list to jsonify
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
+
+
 
 
 
@@ -27,5 +70,5 @@ def index():
 
 
 # Base needed code
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
